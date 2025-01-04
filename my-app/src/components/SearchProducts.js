7528d36a92fe80d43a16de60f06c50952ef2d9b5
suramashaqi <s12323571@stu.najah.edu>
2025-01-04 20:07:02 +0200

    1

---
+++ my-app/src/components/SearchProducts.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SearchProducts ({setSearchQuery}) { 
+  return (
+    <input
+      type="text"
+      placeholder="Search for products..."
+      onChange={(e) => setSearchQuery(e.target.value)}
+    />
+  );
+}
+
+export default SearchProducts;     
