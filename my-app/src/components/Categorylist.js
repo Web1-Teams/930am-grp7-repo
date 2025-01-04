8fa33524286d0d1b5958a44d1941d5a7b0ec2731
ayshamashaqi <s1232648@stu.najah.edu>
2025-01-05 00:04:45 +0300

    1

--- my-app/src/components/Categorylist.js
+++ my-app/src/components/Categorylist.js
@@ -4,8 +4,8 @@ function CategoryList({ categories }) {
   return (
     <div className="category-list">
       {categories.map((category) => (
-        <div key={category.id} className="category-card">
-          <button className="category-button">{category.name}</button>
+        <div key={category.id} className= "category-card" >
+          <button className= "category-button ">{category.name}</button>
         </div>
       ))}
     </div>
